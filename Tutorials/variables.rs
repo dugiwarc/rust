f953efc4c0cc849b107ddba449f7508b62dee9b1
dugiwarc <gicubotnaru@gmail.com>
2021-03-07 12:24:44 +0100

    add variables

---
+++ Tutorials/variables.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let fees = 25_000;
+    let salary:f64 = 35_000.00;
+    let mut mut_fees = 34_000;
+    println!("fees is {} and salary is {} and mutable fees is {}", fees, salary, mut_fees);
+    mut_fees = 22_000;
+    println!("mutable fees is {}",mut_fees);
+    
+}
+
